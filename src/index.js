e2f33c22980a60b9c4456ee117af0ddda1234f9f
Michael Reynolds <michael4reynolds@gmail.com>
2019-08-18 15:52:10 -0400

    Add Radio component
    Make DRY with Radio component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Frame } from "framer"
+
 import "./styles.css"
+
+function Radio(props) {
+  return (
+    <Frame
+      position="relative"
+      size="auto"
+      style={{ display: "flex", marginBottom: 10 }}
+      background="null"
+    >
+      <Frame
+        size={30}
+        shadow="0 1px 5px 0 rgba(0,0,0,0.3)"
+        background="white"
+        borderRadius={15}
+        position="relative"
+        style={{ marginRight: 10 }}
+      />
+      {props.children}
+    </Frame>
+  )
+}
 
 function RadioGroup() {
   return (
     <Frame>
-      <Frame position='relative'>🍏 Apple</Frame>
-      <Frame position='relative'>🥑 Avocado</Frame>
-      <Frame position='relative'>🌶 Pepper</Frame>
+      <Radio>🍏 Apple</Radio>
+      <Radio>🥑 Avocado</Radio>
+      <Radio>🌶 Pepper</Radio>
     </Frame>
   )
 }
