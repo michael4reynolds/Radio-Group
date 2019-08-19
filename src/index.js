74c940ab7350317ffe92c7029fc9623955dcbf46
Michael Reynolds <michael4reynolds@gmail.com>
2019-08-19 11:08:20 -0400

    complete radio group

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
 import { Frame } from "framer"
 
@@ -6,39 +6,66 @@ import "./styles.css"
 
 function Radio(props) {
   return (
-    <Frame
+    <Frame // Radio
       position="relative"
       size="auto"
       style={{ display: "flex", marginBottom: 10 }}
       background="null"
+      {...props}
     >
-      <Frame
+      <Frame // Outer circle
         size={30}
         shadow="0 1px 5px 0 rgba(0,0,0,0.3)"
         background="white"
         borderRadius={15}
         position="relative"
         style={{ marginRight: 10 }}
-      />
-      {props.children}
+      >
+        <Frame // Inner circle
+          size={20}
+          borderRadius={10}
+          background={"purple"}
+          center
+          animate={{ scale: props.selected ? 1 : 0 }}
+        />
+      </Frame>
+      {props.children} {/* Text */}
     </Frame>
   )
 }
 
-function RadioGroup() {
+function RadioGroup({ choices, ...props }) {
+  let [selectedIndex, setSelectedIndex] = useState(0)
   return (
-    <Frame>
-      <Radio>🍏 Apple</Radio>
-      <Radio>🥑 Avocado</Radio>
-      <Radio>🌶 Pepper</Radio>
+    <Frame background={null} {...props}>
+      {choices.map((choice, index) => (
+        <Radio
+          selected={index === selectedIndex}
+          key={choice}
+          onClick={() => {
+            setSelectedIndex(index)
+          }}
+        >
+          {choice}
+        </Radio>
+      ))}
     </Frame>
   )
 }
 
 function App() {
+  let options = [
+    "🍏 Apple",
+    "🥑 Avocado",
+    "🌶 Pepper",
+    "🥕 Carrot",
+    "🍔 Burger",
+    "🥝 Kiwi fruit",
+    "🍌 Banana"
+  ]
   return (
     <div className="App">
-      <RadioGroup />
+      <RadioGroup center choices={options} background="orange" size="auto" />
     </div>
   )
 }
